459a492f8a18044eeaf1004d52fac2bf4fc85895
mohhai1 <160250625+mohhai1@users.noreply.github.com>
2024-11-06 15:23:35 -0500

    add cloud

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:643c3dc891c9fb020007ceabe034bd276d2002a79dc005d30dd1a590856762cd
-size 264
+oid sha256:22e9e156619d60b204a217ddacf20df0970fa5f3bb362299d8f508e365c9205c
+size 260
