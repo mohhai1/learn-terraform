01e49c29cb8b3cf3cb72a9a4f0676071c02a6df8
mohhai1 <mohamadhaidar@hotmail.com>
2024-11-06 15:14:45 -0500

    Update image version

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a0e99b045de478cf26334178fb69dcdfed402ad9573d1753790bd9777b8ad18
+oid sha256:ecf6c540530689731f848744bdeb42322f0a98ccf20619c7ed4c7201e3b8506e
 size 332
