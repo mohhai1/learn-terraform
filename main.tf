ba5c416c92e0b90dce7392708d5a10814a55c237
mohhai1 <mohamadhaidar@hotmail.com>
2024-11-08 18:43:36 -0500

    Commenting in main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5476455083594194e01d4eb7aecd07e6584e7089210889c67e6f868a6a561c51
-size 473
+oid sha256:d63147a3e37208ff29743a8617ceb67787525aaf5ba0c24ddb61de8ec426a51f
+size 477
